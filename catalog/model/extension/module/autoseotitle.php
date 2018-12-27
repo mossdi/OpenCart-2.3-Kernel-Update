1fa14ba928ff03d35cc70a8c0bcae56d8029fbb9
Dmitriy Mosolov <mossol_87@mail.ru>
2018-12-27 19:39:14 +0300

    update

--- catalog/model/extension/module/autoseotitle.php
+++ catalog/model/extension/module/autoseotitle.php
@@ -5,14 +5,14 @@ class ModelExtensionModuleAutoSeoTitle extends Model {
         $seo_title = (array)$this->config->get('autoseotitle_product');
         if (isset($seo_title[$this->config->get('config_language_id')]) && $seo_title[$this->config->get('config_language_id')]) {
             $pattern = array(
-                '[name]'              => $product_info['name'],
-                '[meta_title]'        => $product_info['meta_title'],
-                '[meta_h1]'           => isset($product_info['meta_h1'])?$product_info['meta_h1']?$product_info['meta_h1']:$product_info['name']:'',
-                '[store_name]'        => $this->config->get('config_name'),
-                '[price]'             => $price,
-                '[model]'             => $product_info['model'],
-                '[category_name]'     => !empty($category_info['name'])?$category_info['name']:'',
-                '[manufacturer_name]' => $product_info['manufacturer']
+                '[name]'                => $product_info['name'],
+                '[meta_title]'          => $product_info['meta_title'],
+                '[meta_h1]'             => isset($product_info['meta_h1'])?$product_info['meta_h1']?$product_info['meta_h1']:$product_info['name']:'',
+                '[store_name]'          => $this->config->get('config_name'),
+                '[price]'               => $price,
+                '[model]'               => $product_info['model'],
+                '[category_name]'       => !empty($category_info['name'])?$category_info['name']:'',
+                '[manufacturer_name]'   => $product_info['manufacturer']
             );
 
             if ($this->config->get('autoseotitle_rewrite') || !$product_info['meta_title']) {
@@ -24,14 +24,14 @@ class ModelExtensionModuleAutoSeoTitle extends Model {
         $seo_description = (array)$this->config->get('autoseotitle_descr_product');
         if (isset($seo_description[$this->config->get('config_language_id')]) && $seo_description[$this->config->get('config_language_id')]) {
             $pattern = array(
-                '[name]'              => $product_info['name'],
-                '[meta_title]'        => $product_info['meta_title'],
-                '[meta_h1]'           => isset($product_info['meta_h1'])?$product_info['meta_h1']?$product_info['meta_h1']:$product_info['name']:'',
-                '[store_name]'        => $this->config->get('config_name'),
-                '[price]'             => $price,
-                '[model]'             => $product_info['model'],
-                '[category_name]'     => !empty($category_info['name'])?$category_info['name']:'',
-                '[manufacturer_name]' => $product_info['manufacturer']
+                '[name]'                => $product_info['name'],
+                '[meta_title]'          => $product_info['meta_title'],
+                '[meta_h1]'             => isset($product_info['meta_h1'])?$product_info['meta_h1']?$product_info['meta_h1']:$product_info['name']:'',
+                '[store_name]'          => $this->config->get('config_name'),
+                '[price]'               => $price,
+                '[model]'               => $product_info['model'],
+                '[category_name]'       => !empty($category_info['name'])?$category_info['name']:'',
+                '[manufacturer_name]'   => $product_info['manufacturer']
             );
 
             if ($this->config->get('autoseotitle_descr_rewrite') || !$product_info['meta_description']) {
@@ -43,14 +43,14 @@ class ModelExtensionModuleAutoSeoTitle extends Model {
         $seo_keyword = (array)$this->config->get('autoseotitle_keyw_product');
         if (isset($seo_keyword[$this->config->get('config_language_id')]) && $seo_keyword[$this->config->get('config_language_id')]) {
             $pattern = array(
-                '[name]'              => $product_info['name'],
-                '[meta_title]'        => $product_info['meta_title'],
-                '[meta_h1]'           => isset($product_info['meta_h1'])?$product_info['meta_h1']?$product_info['meta_h1']:$product_info['name']:'',
-                '[store_name]'        => $this->config->get('config_name'),
-                '[price]'             => $price,
-                '[model]'             => $product_info['model'],
-                '[category_name]'     => !empty($category_info['name'])?$category_info['name']:'',
-                '[manufacturer_name]' => $product_info['manufacturer']
+                '[name]'                => $product_info['name'],
+                '[meta_title]'          => $product_info['meta_title'],
+                '[meta_h1]'             => isset($product_info['meta_h1'])?$product_info['meta_h1']?$product_info['meta_h1']:$product_info['name']:'',
+                '[store_name]'          => $this->config->get('config_name'),
+                '[price]'               => $price,
+                '[model]'               => $product_info['model'],
+                '[category_name]'       => !empty($category_info['name'])?$category_info['name']:'',
+                '[manufacturer_name]'   => $product_info['manufacturer']
             );
 
             if ($this->config->get('autoseotitle_keyw_rewrite') || !$product_info['meta_keyword']) {
@@ -61,9 +61,9 @@ class ModelExtensionModuleAutoSeoTitle extends Model {
 
         if ($ajax) {
             $meta_data = array(
-                'meta_title'       => !empty($meta_title) ? $meta_title : $product_info['meta_title'],
-                'meta_description' => !empty($meta_description) ? $meta_description : $product_info['meta_description'],
-                'meta_keyword'     => !empty($meta_keyword) ? $meta_keyword : $product_info['meta_keyword']
+                'meta_title'        => !empty($meta_title) ? $meta_title : $product_info['meta_title'],
+                'meta_description'  => !empty($meta_description) ? $meta_description : $product_info['meta_description'],
+                'meta_keyword'      => !empty($meta_keyword) ? $meta_keyword : $product_info['meta_keyword']
             );
 
             return $meta_data;
@@ -128,14 +128,14 @@ class ModelExtensionModuleAutoSeoTitle extends Model {
             }
 
             $sort_title = array(
-                'manufacturer_title' => $manufacturer_title,
-                'in_stock_title'     => $in_stock_title,
-                'price_gap_title'    => $price_gap_title,
-                'attribute_title'    => $attribute_title,
-                'sort_price_title'   => $sort_price_title,
-                'sort_name_title'    => $sort_name_title,
-                'limit_title'        => $limit_title,
-                'filter_title'       => $filter_title
+                'manufacturer_title'    => $manufacturer_title,
+                'in_stock_title'        => $in_stock_title,
+                'price_gap_title'       => $price_gap_title,
+                'attribute_title'       => $attribute_title,
+                'sort_price_title'      => $sort_price_title,
+                'sort_name_title'       => $sort_name_title,
+                'limit_title'           => $limit_title,
+                'filter_title'          => $filter_title
             );
 
             $page_title = (array)$this->config->get('autoseotitle_page');
@@ -221,21 +221,38 @@ class ModelExtensionModuleAutoSeoTitle extends Model {
             $manufacturer_info['meta_keyword'] = !empty($manufacturer_info['meta_keyword']) ? $manufacturer_info['meta_keyword'] : false;
 
             $pattern = array(
-                '[name]'       => $manufacturer_info['name'],
-                '[meta_h1]'    => $manufacturer_info['meta_h1'],
-                '[meta_title]' => $manufacturer_info['meta_title'],
-                '[store_name]' => $this->config->get('config_name'),
+                '[name]'        => $manufacturer_info['name'],
+                '[meta_h1]'     => $manufacturer_info['meta_h1'],
+                '[meta_title]'  => $manufacturer_info['meta_title'],
+                '[store_name]'  => $this->config->get('config_name'),
             );
 
-            if ((isset($this->request->get['sort']) && $this->request->get['sort'] == 'cd.name') && (isset($this->request->get['order']) && $this->request->get['order'] == 'DESC')) {
+            if (isset($this->request->get['in_stock']) && $this->request->get['in_stock'] == true ) {
+                $in_stock_title = ' | ' . $this->language->get('entry_in_stock');
+            } else {
+                $in_stock_title = '';
+            }
+            if (isset($this->request->get['min_price']) && isset($this->request->get['max_price'])) {
+                $price_gap_title = ' | ' . $this->language->get('text_from') . ' ' . $this->currency->format($this->request->get['min_price'], $this->session->data['currency']) . ' ' . $this->language->get('text_to') . ' ' . $this->currency->format($this->request->get['max_price'], $this->session->data['currency']);
+            } else {
+                $price_gap_title = '';
+            }
+            if ((isset($this->request->get['sort']) && $this->request->get['sort'] == 'price') && (isset($this->request->get['order']) && $this->request->get['order'] == 'DESC')) {
+                $sort_price_title = ' | ' . $this->language->get('text_sort') . ' ' . $this->language->get('text_price_desc');
+            } elseif ((isset($this->request->get['sort']) && $this->request->get['sort'] == 'price') && (isset($this->request->get['order']) && $this->request->get['order'] == 'ASC')) {
+                $sort_price_title = ' | ' . $this->language->get('text_sort') . ' ' .  $this->language->get('text_price_asc');
+            } else {
+                $sort_price_title = '';
+            }
+            if ((isset($this->request->get['sort']) && $this->request->get['sort'] == 'name') && (isset($this->request->get['order']) && $this->request->get['order'] == 'DESC')) {
                 $sort_name_title = ' | ' . $this->language->get('text_sort') . ' ' . $this->language->get('text_name_desc');
-            } elseif ((isset($this->request->get['sort']) && $this->request->get['sort'] == 'cd.name') && (isset($this->request->get['order']) && $this->request->get['order'] == 'ASC')) {
+            } elseif ((isset($this->request->get['sort']) && $this->request->get['sort'] == 'name') && (isset($this->request->get['order']) && $this->request->get['order'] == 'ASC')) {
                 $sort_name_title = ' | ' . $this->language->get('text_sort') . ' ' .  $this->language->get('text_name_asc');
             } else {
                 $sort_name_title = '';
             }
             if (isset($this->request->get['limit'])) {
-                $limit_title = ' | ' . $this->language->get('text_limit') .  ' ' . $this->request->get['limit'];
+                $limit_title = ' | ' . $this->language->get('text_limit') . ' ' . $this->request->get['limit'];
             } else {
                 $limit_title = '';
             }
@@ -244,17 +261,14 @@ class ModelExtensionModuleAutoSeoTitle extends Model {
             } else {
                 $filter_title = '';
             }
-            if (isset($this->request->get['in_stock']) && $this->request->get['in_stock'] == true ) {
-                $in_stock_title = ' | ' . $this->language->get('entry_in_stock');
-            } else {
-                $in_stock_title = '';
-            }
 
             $sort_title = array(
-                'sort_name_title'    => $sort_name_title,
-                'limit_title'        => $limit_title,
-                'filter_title'       => $filter_title,
-                'in_stock_title'     => $in_stock_title
+                'in_stock_title'    => $in_stock_title,
+                'price_gap_title'   => $price_gap_title,
+                'sort_price_title'  => $sort_price_title,
+                'sort_name_title'   => $sort_name_title,
+                'limit_title'       => $limit_title,
+                'filter_title'      => $filter_title,
             );
 
             $page_title = (array)$this->config->get('autoseotitle_page');
@@ -278,10 +292,10 @@ class ModelExtensionModuleAutoSeoTitle extends Model {
         $seo_description = (array)$this->config->get('autoseotitle_descr_manufacturer');
         if (isset($seo_description[$this->config->get('config_language_id')]) && $seo_description[$this->config->get('config_language_id')]) {
             $pattern = array(
-                '[name]'              => $manufacturer_info['name'],
-                '[meta_h1]'           => $manufacturer_info['meta_h1'],
-                '[meta_title]'        => $manufacturer_info['meta_title'],
-                '[store_name]'        => $this->config->get('config_name'),
+                '[name]'        => $manufacturer_info['name'],
+                '[meta_h1]'     => $manufacturer_info['meta_h1'],
+                '[meta_title]'  => $manufacturer_info['meta_title'],
+                '[store_name]'  => $this->config->get('config_name'),
             );
 
             $page_title = (array)$this->config->get('autoseotitle_page');
@@ -305,10 +319,10 @@ class ModelExtensionModuleAutoSeoTitle extends Model {
         $seo_keyword = (array)$this->config->get('autoseotitle_keyw_manufacturer');
         if (isset($seo_keyword[$this->config->get('config_language_id')]) && $seo_keyword[$this->config->get('config_language_id')]) {
             $pattern = array(
-                '[name]'              => $manufacturer_info['name'],
-                '[meta_h1]'           => $manufacturer_info['meta_h1'],
-                '[meta_title]'        => $manufacturer_info['meta_title'],
-                '[store_name]'        => $this->config->get('config_name'),
+                '[name]'        => $manufacturer_info['name'],
+                '[meta_h1]'     => $manufacturer_info['meta_h1'],
+                '[meta_title]'  => $manufacturer_info['meta_title'],
+                '[store_name]'  => $this->config->get('config_name'),
             );
 
             $page_title = (array)$this->config->get('autoseotitle_page');
