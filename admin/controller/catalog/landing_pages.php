db70fd6e7fead82b78fd391c4c7abb0af35075c1
Dmitriy Mosolov <mossol_87@mail.ru>
2019-02-09 11:18:08 +0300

    landing pages alfa 1.1

--- admin/controller/catalog/landing_pages.php
+++ admin/controller/catalog/landing_pages.php
@@ -4,9 +4,47 @@ class ControllerCatalogLandingPages extends Controller
     private $error = array();
 
     public function index() {
+        $this->load->language('catalog/landing_pages');
+
+        $this->document->setTitle($this->language->get('heading_title'));
+
         $this->load->model('catalog/landing_pages');
 
-        $this->load->language('catalog/landing_pages');
+        $this->getList();
+    }
+
+    protected function getList() {
+        if (isset($this->request->get['sort'])) {
+            $sort = $this->request->get['sort'];
+        } else {
+            $sort = 'name';
+        }
+
+        if (isset($this->request->get['order'])) {
+            $order = $this->request->get['order'];
+        } else {
+            $order = 'ASC';
+        }
+
+        if (isset($this->request->get['page'])) {
+            $page = $this->request->get['page'];
+        } else {
+            $page = 1;
+        }
+
+        $url = '';
+
+        if (isset($this->request->get['sort'])) {
+            $url .= '&sort=' . $this->request->get['sort'];
+        }
+
+        if (isset($this->request->get['order'])) {
+            $url .= '&order=' . $this->request->get['order'];
+        }
+
+        if (isset($this->request->get['page'])) {
+            $url .= '&page=' . $this->request->get['page'];
+        }
 
         $data['breadcrumbs'] = array();
 
@@ -17,18 +55,107 @@ class ControllerCatalogLandingPages extends Controller
 
         $data['breadcrumbs'][] = array(
             'text' => $this->language->get('heading_title'),
-            'href' => $this->url->link('catalog/landing_pages', 'token=' . $this->session->data['token'], true)
+            'href' => $this->url->link('catalog/landing_pages', 'token=' . $this->session->data['token'] . $url, true)
         );
+
+        $data['add'] = $this->url->link('catalog/landing_pages/add', 'token=' . $this->session->data['token'] . $url, true);
+        $data['delete'] = $this->url->link('catalog/landing_pages/delete', 'token=' . $this->session->data['token'] . $url, true);
+
+        $data['landing_pages'] = array();
+
+        $filter_data = array(
+            'sort'  => $sort,
+            'order' => $order,
+            'start' => ($page - 1) * $this->config->get('config_limit_admin'),
+            'limit' => $this->config->get('config_limit_admin')
+        );
+
+        $layout_total = $this->model_design_layout->getTotalLayouts();
+
+        $results = $this->model_design_layout->getLayouts($filter_data);
+
+        foreach ($results as $result) {
+            $data['landing_pages'][] = array(
+                'layout_id' => $result['layout_id'],
+                'name'      => $result['name'],
+                'edit'      => $this->url->link('design/layout/edit', 'token=' . $this->session->data['token'] . '&layout_id=' . $result['layout_id'] . $url, true)
+            );
+        }
 
         $data['heading_title'] = $this->language->get('heading_title');
 
-        $data['landing_pages'] = $this->model_catalog_landing_pages->getLandingPages();
+        $data['text_list'] = $this->language->get('text_list');
+        $data['text_no_results'] = $this->language->get('text_no_results');
+        $data['text_confirm'] = $this->language->get('text_confirm');
+
+        $data['column_name'] = $this->language->get('column_name');
+        $data['column_action'] = $this->language->get('column_action');
+
+        $data['button_add'] = $this->language->get('button_add');
+        $data['button_edit'] = $this->language->get('button_edit');
+        $data['button_delete'] = $this->language->get('button_delete');
+
+        if (isset($this->error['warning'])) {
+            $data['error_warning'] = $this->error['warning'];
+        } else {
+            $data['error_warning'] = '';
+        }
+
+        if (isset($this->session->data['success'])) {
+            $data['success'] = $this->session->data['success'];
+
+            unset($this->session->data['success']);
+        } else {
+            $data['success'] = '';
+        }
+
+        if (isset($this->request->post['selected'])) {
+            $data['selected'] = (array)$this->request->post['selected'];
+        } else {
+            $data['selected'] = array();
+        }
+
+        $url = '';
+
+        if ($order == 'ASC') {
+            $url .= '&order=DESC';
+        } else {
+            $url .= '&order=ASC';
+        }
+
+        if (isset($this->request->get['page'])) {
+            $url .= '&page=' . $this->request->get['page'];
+        }
+
+        $data['sort_name'] = $this->url->link('design/layout', 'token=' . $this->session->data['token'] . '&sort=name' . $url, true);
+
+        $url = '';
+
+        if (isset($this->request->get['sort'])) {
+            $url .= '&sort=' . $this->request->get['sort'];
+        }
+
+        if (isset($this->request->get['order'])) {
+            $url .= '&order=' . $this->request->get['order'];
+        }
+
+        $pagination = new Pagination();
+        $pagination->total = $layout_total;
+        $pagination->page = $page;
+        $pagination->limit = $this->config->get('config_limit_admin');
+        $pagination->url = $this->url->link('design/layout', 'token=' . $this->session->data['token'] . $url . '&page={page}', true);
+
+        $data['pagination'] = $pagination->render();
+
+        $data['results'] = sprintf($this->language->get('text_pagination'), ($layout_total) ? (($page - 1) * $this->config->get('config_limit_admin')) + 1 : 0, ((($page - 1) * $this->config->get('config_limit_admin')) > ($layout_total - $this->config->get('config_limit_admin'))) ? $layout_total : ((($page - 1) * $this->config->get('config_limit_admin')) + $this->config->get('config_limit_admin')), $layout_total, ceil($layout_total / $this->config->get('config_limit_admin')));
+
+        $data['sort'] = $sort;
+        $data['order'] = $order;
 
         $data['header'] = $this->load->controller('common/header');
         $data['column_left'] = $this->load->controller('common/column_left');
         $data['footer'] = $this->load->controller('common/footer');
 
-        $this->response->setOutput($this->load->view('catalog/landing_pages', $data));
+        $this->response->setOutput($this->load->view('design/layout_list', $data));
     }
 }
-    
--- admin/model/catalog/landing_pages.php
+++ admin/model/catalog/landing_pages.php
@@ -1,10 +1,39 @@
 <?php
 class ModelCatalogLandingPages extends Model {
-    public function getLandingPages() {
-        $query = $this->db->query("
-            SELECT * FROM " . DB_PREFIX . "landing_page lp
-            LEFT JOIN " . DB_PREFIX . "landing_page_description lpd ON (lp.landing_id = lpd.landing_id)
-        ");
+    public function getLandingPages($data = array()) {
+        $language_id = $this->config->get('config_admin_language');
+
+        $sql = "SELECT * FROM " . DB_PREFIX . "landing_page lp
+                LEFT JOIN " . DB_PREFIX . "landing_page_description lpd ON (lp.landing_id = lpd.landing_id AND lpd.language_id = " . $language_id . ")
+                ";
+
+        $sort_data = array('name');
+
+        if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
+            $sql .= " ORDER BY " . $data['sort'];
+        } else {
+            $sql .= " ORDER BY name";
+        }
+
+        if (isset($data['order']) && ($data['order'] == 'DESC')) {
+            $sql .= " DESC";
+        } else {
+            $sql .= " ASC";
+        }
+
+        if (isset($data['start']) || isset($data['limit'])) {
+            if ($data['start'] < 0) {
+                $data['start'] = 0;
+            }
+
+            if ($data['limit'] < 1) {
+                $data['limit'] = 20;
+            }
+
+            $sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
+        }
+
+        $query = $this->db->query($sql);
 
         return $query->rows;
     }
