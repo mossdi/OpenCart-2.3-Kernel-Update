e87db85551048a369eced302049a6c3c53615d48
dimoss <mossol_87@mail.ru>
2019-06-22 18:38:43 +0300

    update

--- admin/view/javascript/service.js
+++ admin/view/javascript/service.js
@@ -1,20 +1,11 @@
 function createParentProduct(token) {
-    var selectedProduct = $('#form-product').serialize();
+    let selectedProduct = $('#form-product').serialize();
 
     if (selectedProduct === "") return alert("Вы не выбрали ни одного товара из списка!");
 
     $.magnificPopup.open({
         items: {
             src: 'index.php?route=service/create_parent_product/index&token=' + token + '&' + selectedProduct,
-            type: 'ajax'
-        }
-    });
-}
-
-function quickEditProduct(product_id, token) {
-    $.magnificPopup.open({
-        items: {
-            src: 'index.php?route=service/quick_edit_product/index&product_id=' + product_id + '&token=' + token,
             type: 'ajax'
         }
     });
